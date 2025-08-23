b7791c5b2d768cbfee4f00a3dabf4b7f038b7f98
vedant-dev <vedantdbhamare@gmail.com>
2025-08-23 14:39:05 +0530

    updated the navigator

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from '../screens/LoginScreen';
 import HomeScreen from '../screens/HomeScreen';
+import CreateTaskScreen from '../screens/CreateTaskScreen';
 import type { RootStackParamList } from './types';
 
 const Stack = createStackNavigator<RootStackParamList>();
@@ -10,6 +11,7 @@ const AppNavigator = () => (
   <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
     <Stack.Screen name="Login" component={LoginScreen} />
     <Stack.Screen name="Home" component={HomeScreen} />
+    <Stack.Screen name="CreateTask" component={CreateTaskScreen} />
   </Stack.Navigator>
 );
 
